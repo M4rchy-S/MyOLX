c3173311b2da19c0c028f9e237c5699d4004d3e7
M4rchy-S <alex12345suslik@gmail.com>
2024-12-26 23:53:06 +0200

    Making settings to work

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def db_query(query: str) -> str:
         return rows
     except Exception as e:
         print("[ERROR] Query database error acquired")
+        conn.rollback()
         return ""
     finally:
         if conn:
@@ -55,7 +56,7 @@ def get_hash_password(input_password: str) -> str:
 
 @app.errorhandler(Exception)
 def handle_error(e):
-    return "<h3>Error 500 on server Side</h3>"
+    return f"<h3>Error 500 on server Side</h3><\br>{e}"
 
 @app.route("/")
 @app.route("/index")
@@ -79,7 +80,7 @@ def postpage(post_id):
 
 @app.route("/error")
 def error_page():
-    return "<h2>Error 404</h2>"
+    return "<h2>Error 404 - Page not found</h2>"
 
 @app.route("/sign-in")
 def sign_in():
@@ -108,14 +109,23 @@ def create_ann():
 @app.route("/settings")
 def settings():
     if session:
-        return render_template("settings.html")
+        message = request.args.get('message', '')
+        return render_template("settings.html", error_message=message)
     else:
         return redirect(url_for('sign_in'))
 
 @app.route("/mylist")
 def ann_list():
     if session:
-        return render_template("ann-list.html")
+        res = db_query(f"SELECT posts.title, posts.price, posts.id FROM posts INNER JOIN users ON posts.user_id = users.id WHERE users.email = '{session['email']}'")
+        if res == '':
+            return redirect(url_for('handle_error'))
+        # titles = res[0]
+        # prices = res[1]
+        # ids = res[2]
+        # summary_data = zip(titles, prices, ids)
+        print(f"[DEBUG] {res=}")
+        return render_template("ann-list.html", lines_data=res)
     else:
         return redirect(url_for('sign_in'))
 
@@ -134,9 +144,12 @@ def create_account():
         
         res = db_query(f"INSERT INTO users (name, phone, email, password) VALUES ('{user_name}', '+{phone}', '{mail}', '{password}')")
         if res == '':
-            return redirect(url_for('handle_error'))
+            return redirect(url_for('sign_up', message="Phone number or Email are already busy"))
         
         session['name'] = user_name 
+        session['email'] = mail
+        session['password'] = password
+        session['phone'] = phone
         return redirect(url_for('ann_list')) 
     else:
         return redirect(url_for('main'))
@@ -146,12 +159,14 @@ def enter_account():
     if request.method == 'POST':
 
         email = request.form['email']
-        # password = pbkdf2_hmac('sha256', request.form['password'].encode('utf-8'), b'SuperSecretSalt', 500).hex()
         password = get_hash_password(request.form['password'])
         
-        res = db_query(f"SELECT users.\"name\" FROM users WHERE users.email = '{email}' AND users.\"password\" = '{password}'")
+        res = db_query(f"SELECT users.\"name\", users.email, users.\"password\", users.phone FROM users WHERE users.email = '{email}' AND users.\"password\" = '{password}'")
         if res:
             session['name'] = res[0][0]
+            session['email'] = res[0][1]
+            session['password'] = res[0][2]
+            session['phone'] = res[0][3]
             return redirect(url_for('ann_list'))  
         elif res == '':
             return redirect(url_for('handle_error'))      
@@ -165,6 +180,77 @@ def enter_account():
 def log_out():
     session.clear()
     return redirect(url_for('main'))
+
+@app.route("/change-password", methods=['POST'])
+def change_password():
+    if session and request.method == 'POST':
+        if get_hash_password(request.form['old_password']) == session['password']:
+            iv = InputValidation()
+            iv.validate_password(request.form['new_password'])
+            if iv.count() == 0:
+                new_password = get_hash_password(request.form['new_password'])
+                res = db_query(f"UPDATE users SET password = '{new_password}' WHERE users.email = '{session['email']}'")
+                if res:
+                    session['password'] = new_password
+                    return redirect(url_for('settings'))
+                elif res == '':
+                    return redirect(url_for('handle_error'))      
+                else:
+                    return redirect(url_for('settings', message="Error"))
+            else:
+                return redirect(url_for('settings', message="Wrong new password requirements"))
+        else:
+            return redirect(url_for('settings', message="Wrong old password"))
+        
+    else:
+        return redirect(url_for('main'))
+
+@app.route("/change-phone", methods=['POST'])
+def change_phone():
+    if session and request.method == 'POST':
+        new_phone_number = request.form['new_phone']
+        iv = InputValidation()
+        phone_number = iv.validate_phone(new_phone_number)
+        if iv.count() > 0:
+            return redirect(url_for('settings', message=iv.getfirst()))
+        else:
+            res = db_query(f"SELECT users.phone FROM users WHERE users.phone = '+{phone_number}'")
+            if len(res) == 0:
+                #   UPDATE users SET phone = '+36' WHERE users.email = 'testsubject@mail2.com'
+                res = db_query(f"UPDATE users SET phone = '+{phone_number}' WHERE users.email = '{session['email']}'")
+                if res == '':
+                    return redirect(url_for('handle_error'))
+                else:  
+                    return redirect(url_for('settings'))
+            elif len(res) > 0:
+                return redirect(url_for('settings', message="This phone number is already busy"))
+            elif res == '':
+                return redirect(url_for('handle_error'))  
+            else:
+                return redirect(url_for('settings', message="Error"))
+    else:
+        return redirect(url_for('main'))
+
+@app.route("/delete-account", methods=['POST'])
+def delete_account():
+    if session and request.method == 'POST':
+        res = db_query(f"SELECT id FROM users WHERE email = '{session['email']}'")
+        if res == '':
+            return redirect(url_for('handle_error')) 
+        
+        user_id = res[0][0]
+
+        res = db_query(f"DELETE FROM posts WHERE user_id = {user_id}")
+        if res == '':
+            return redirect(url_for('handle_error'))
+
+        res = db_query(f"DELETE FROM users WHERE email = '{session['email']}'")
+        if res == '':
+            return redirect(url_for('handle_error')) 
+        else:
+            return redirect(url_for('log_out'))
+    else:
+        return redirect(url_for('main'))
     
 
 
